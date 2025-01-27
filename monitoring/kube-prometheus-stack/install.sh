c0ed042f0bcd5a6ec3d1eb13496e7490d6dfae29
tw1zr <tw1zr@efym.net>
2025-01-27 13:37:01 +0000

    remove gatus for now, install kube-prometheus-stack using kustomize

--- monitoring/kube-prometheus-stack/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
