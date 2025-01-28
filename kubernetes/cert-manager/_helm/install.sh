55eb5f0b352a38ea55a443c650b4160e1c621204
tw1zr <tw1zr@efym.net>
2025-01-28 11:46:49 +0000

    switch to sealed-secrets and add resting apps to argocd

--- kubernetes/cert-manager/_helm/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-kubectl create namespace cert-manager
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.crds.yaml
-helm install cert-manager jetstack/cert-manager --namespace cert-manager --values=values.yaml --version v1.9.1
